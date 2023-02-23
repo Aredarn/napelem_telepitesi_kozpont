e23b8bc625cce1138ad345312bf82b6efc669216
Martin M <65924165+Aredarn@users.noreply.github.com>
2023-02-23 11:34:31 +0100

    v0.0
    Első push a repoban,csupán tesztelés céljából

---
+++ Fomenu.cs
@@ -0,0 +1,10 @@
+namespace napelem_telepito_kozpont
+{
+    public partial class Fomenu : Form
+    {
+        public Fomenu()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+namespace napelem_telepito_kozpont
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Fomenu());
+        }
+    }
+}
