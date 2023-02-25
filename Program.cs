8da23acb1502c7a4927807d4b8fb1d8cbbafddd3
Holler János <hollerjanika@gmail.com>
2023-02-25 19:58:26 +0100

    Test

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ namespace napelem_telepito_kozpont
         static void Main()
         {
             // To customize application configuration such as set high DPI settings or default font,
+            // JANI
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Fomenu());
