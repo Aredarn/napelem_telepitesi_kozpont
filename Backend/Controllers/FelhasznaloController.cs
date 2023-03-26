64df3d2937ae67bfbe0d58dcb01074388e28b0d9
Holler János <hollerjanika@gmail.com>
2023-03-26 07:38:19 +0200

    Bejelentkezés megvalósítása
    A `FelhasznaloController` létrehozása, hogy a metódusokat könnyen lehessen használni, illetve elérni.
    A `Hash()` metódus implementálása.
    Bejelentkezés metódus elkészítése, mely a `Hash()` metódus segítségével fog visszatérni a megfelelő felhasználóval.
    
    Ha a bejelentkezés sikeres volt, akkor a felhasználó `Role`-ja alapján fog megjelenni számára a megfelelő ablak. Különben a felhasználó a hibáról értesítést kap.

---
+++ Backend/Controllers/FelhasznaloController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using napelem_telepito_kozpont.Backend.DatabaseConnection;
+using napelem_telepito_kozpont.Backend.Modells_Tables;
+using System.Security.Cryptography;
+using System.Security.Policy;
+
+namespace napelem_telepito_kozpont.Backend.Controllers
+{
+    public class FelhasznaloController
+    {
+        /* Enkripteli a paraméterben megadott string-et, majd visszatér
+           az enkriptelt változattal. */
+        private string Hash(string value)
+        {
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(value);
+
+            byte[] hashBytes;
+
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                hashBytes = sha256.ComputeHash(passwordBytes);
+            }
+
+            return Convert.ToBase64String(hashBytes);
+        }
+
+        /* A `Login` metódus azt a célt szolgálja, hogy meg lehessen vizsgálni
+           a megadott két paraméter (`felhasználónév`, `jelszó`) alapján
+           létezik-e felhasználó az adatbázisban, mely a két paraméter Hash-elt
+           értékeivel rendelkezik. Ha létezik, akkor visszatér egy `Felhasznalo`
+           objektummal, mely tartalmazza az értékeket, különben csak egy dummy
+           `Felhasznalo` objektumot ad vissza. */
+        public Felhasznalo Login(string username, string password)
+        {
+            try
+            {
+                /* Adatbázis kapcsolat. */
+                NapelemDbContext context = new();
+
+                /* A paraméterben megadott változók értékeinek Hash-elése. */
+                string usernameHash = Hash(username);
+                string passwordHash = Hash(password);
+
+                /* Megvizsgálni, hogy az előbb Hash-elt felhasználó és jelszó
+                   létezik-e az adatbázisban. Ha igen, akkor tárolja egy
+                   `Felhasznalo` objektumban, majd térjen is vissza vele. */
+                Felhasznalo felhasznalo = context.Felhasznalo
+                    .Single(row => row.Name == usernameHash
+                                && row.Password == passwordHash);
+
+                /* Térjen vissza az előbb létrehozott `Felhasznalo` objektummal. */
+                return felhasznalo;
+            }
+            catch (Exception)
+            {
+                /* Bármilyen hiba, illetve kivétel esetén a program térjen
+                   vissza egy olyan `Felhasznalo` objektummal, melyben a
+                   tulajdonságok üres string-ek, illetve 0-ák, annak érdekében,
+                   hogy fel lehessen ismerni, hogy nem egy valós felhasználó
+                   lett megtalálva. */
+                return new Felhasznalo {
+                    Name = "",
+                    Password = "",
+                    Role = 0
+                };
+            }
+        }
+    }
+}
--- GUI_Forms/Login.cs
+++ GUI_Forms/Login.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using napelem_telepito_kozpont.Backend.Modells_Tables;
+using napelem_telepito_kozpont.Backend.Controllers;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -13,6 +15,8 @@ namespace napelem_telepito_kozpont.GUI_Forms
 {
     public partial class Login : Form
     {
+        public Felhasznalo felhasznalo;
+
         public Login()
         {
             InitializeComponent();
@@ -25,9 +29,82 @@ namespace napelem_telepito_kozpont.GUI_Forms
         }
 
 
-        //kiválasztott érték (combobox) alapján navigálás
+        /* `Belépés` gomb eventje: */
         private void buttonBelepes_Click(object sender, EventArgs e)
         {
+            try
+            {
+                /* `FelhasznaloController` létrehozása, hogy később a `Login()`
+                   metódusát használni lehessen. */
+                FelhasznaloController felhasznaloController = new();
+
+                /* A begépelt felhasználónév & jelszó értékek változóba
+                   való mentése. */
+                string username = textBoxFelhasznalonev.Text;
+                string password = textBoxJelszo.Text;
+
+                /* Validáció annak érdekében, hogy a felhasználó értesítést
+                   kapjon, ha bármelyik mező üresen maradt. */
+                if (username == "")
+                {
+                    throw new Exception("Felhasználónév nem lehet üres!");
+                }
+                else if (password == "")
+                {
+                    throw new Exception("Jelszó nem lehet üres!");
+                }
+
+                /* Megvizsgálásra kerül, hogy létezik-e felhasználó a korábban
+                   mentett felhasználó, illetve jelszó értékekkel. */
+                felhasznalo = felhasznaloController.Login(
+                    username,
+                    password
+                );
+
+                /* A következő rész fog abban segíteni, hogy a megfelelő
+                   `Role`-nak a megfelelő ablak nyíljon meg. */
+                switch (felhasznalo.Role)
+                {
+                    case 4: // Admin
+                        Admin admin = new Admin();
+                        admin.Show();
+                        break;
+
+                    case 3: // Raktárvezető
+                        Raktárvezető raktarvezeto = new Raktárvezető();
+                        raktarvezeto.Show();
+                        break;
+
+                    case 2: // Raktáros
+                        Raktaros raktaros = new Raktaros();
+                        raktaros.Show();
+                        break;
+
+                    case 1: // Szakember
+                        Szakember szakember = new Szakember();
+                        szakember.Show();
+                        break;
+
+                    default:
+                    case 0: // Helytelen `role`
+                        throw new Exception("Felhasználónév vagy jelszó helytelen!");
+                }
+
+                /* Zárja be az aktuális ablakot! */
+                this.Hide();
+            }
+            catch (Exception exception)
+            {
+                /* Bármilyen hiba, vagy kivétel esetén a felhasználó értesítve lesz! */
+                MessageBox.Show($"Bejelentkezés sikertelen. ('{exception.Message}')");
+            }
+
+            /*
+
+            felhasznalo = new FelhasznaloController.Login(
+                textBoxJelszo.Text,
+                textBoxJelszo.Text
+            );
             string be = comboBox1.Text;
             
             if (be == "Szakember")
@@ -58,6 +135,8 @@ namespace napelem_telepito_kozpont.GUI_Forms
                 a1.Show();
                 this.Hide();
             }
+
+            */
             
         }
     }
