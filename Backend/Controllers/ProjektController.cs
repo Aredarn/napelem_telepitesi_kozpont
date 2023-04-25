06f829cd75a5d959e7086a88c24291e30a78d215
Holler János <hollerjanika@gmail.com>
2023-04-25 21:59:38 +0200

    Projekt kontroller létrehozása
    A `ProjektLetrehozasa()` metódus implementálása.

---
+++ Backend/Controllers/ProjektController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using napelem_telepito_kozpont.Backend.DatabaseConnection;
+using napelem_telepito_kozpont.Backend.Modells_Tables;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
+namespace napelem_telepito_kozpont.Backend.Controllers
+{
+    public class ProjektController
+    {
+        public bool ProjektLetrehozasa(Projektek projekt)
+        {
+            try
+            {
+                NapelemDbContext context = new();
+
+                context.Projekt.Add(projekt);
+                context.SaveChanges();
+
+                return true;
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+
+                return false;
+            }
+
+        }
+    }
+}
